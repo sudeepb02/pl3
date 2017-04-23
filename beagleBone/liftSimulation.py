84884ab645f4a185cddfa2ad9cb7bbb0e3fd742f
sudeepb02 <sudeepbiswas02@gmail.com>
2017-04-23 15:07:24 +0000

    Add up and down leds with animation

--- beagleBone/liftSimulation.py
+++ beagleBone/liftSimulation.py
@@ -1,68 +1,97 @@
-import Adafruit_BBIO.GPIO as GPIO
+import Adafruit_BBIO.GPIO as g
 import time as t
 
-#Initialize all 7-segment display
-ZERO = ['P8_11', 'P8_12', 'P8_13', 'P8_14', 'P8_15', 'P8_16']
-ONE = ['P8_12', 'P8_13']
-TWO = ['P8_11', 'P8_12', 'P8_17', 'P8_15', 'P8_14']
-THREE = ['P8_11', 'P8_12', 'P8_17', 'P8_13', 'P8_14']
-LL = ['P9_23', 'P9_24', 'P9_11', 'P9_12', 'P9_13', 'P9_14', 'P9_15', 'P9_16']
-SSD= ['P8_11', 'P8_12', 'P8_13', 'P8_14', 'P8_15', 'P8_16', 'P8_17']
+//Define led, buttons and ssd
 
-#Function to clear display
-def clear():
-        for i in range(0, len(SSD)):
-                GPIO.output(SSD[i], GPIO.HIGH)
+upled = ['P9_11', 'P9_13', 'P9_15', 'P9_23']
+downled = ['P9_12', 'P9_14', 'P9_16', 'P9_24']
 
-#Initialize and turn off all LED Lights
-for i in range(0, len(LL)):
-        GPIO.setup(LL[i], GPIO.OUT)
-        GPIO.output(LL[i], GPIO.LOW)
+buttons = ['P8_7', 'P8_8', 'P8_9', 'P8_10']
 
-#Initialize and turn off all 7 segment display
-for i in range(0, len(SSD)):
-        GPIO.setup(SSD[i], GPIO.OUT)
-        GPIO.output(SSD[i], GPIO.HIGH)
+ssd = ['P8_11', 'P8_12', 'P8_13', 'P8_14', 'P8_15', 'P8_16', 'P8_17']
+zero = ['P8_11', 'P8_12', 'P8_13', 'P8_14', 'P8_15', 'P8_16']
+one = ['P8_12', 'P8_13']
+two = ['P8_11', 'P8_12', 'P8_17', 'P8_14', 'P8_15']
+three = ['P8_11', 'P8_12', 'P8_13', 'P8_14', 'P8_17']
 
-#Initialize input keys
-INP = ['P8_7', 'P8_8', 'P8_9', 'P8_10']
-for i in range(0, len(INP)):
-        GPIO.setup(INP[i], GPIO.IN)
+//Initialize 
+for i in range(len(upled)):
+	g.setup(upled[i], g.OUT)
+	g.setup(downled[i], g.OUT)
+	g.setup(buttons[i], g.IN)
 
-#Set the current floor number
-currFloor = 'P8_9'
-prevFloor = 'P8_9'
+	g.output(upled[i], g.LOW)
+	g.output(downled[i], g.LOW)
+
+for i in range(len(ssd)):
+	g.setup(ssd[i], g.OUT)
+
+curr = 0
+dest = 0
+
+def clearled():
+	for i in range(len(ssd)):
+		g.output(ssd[i], g.HIGH)
+
+def display(x):
+	clearled()
+
+	if(x==0):
+		for i in range(len(zero)):
+			g.output(zero[i], g.LOW)
+	elif(x==1):
+		for i in range(len(one)):
+			g.output(one[i], g.LOW)
+	elif(x==2):
+		for i in range(len(two)):
+			g.output(two[i], g.LOW)
+	elif(x==3):
+		for i in range(len(three)):
+			g.output(three[i], g.LOW)
+
+def disp(curr, dest):
+	if(curr < dest):
+		for i in range(len(upled)):
+			g.output(upled[i], g.HIGH)
+			g.output(downled[i], g.LOW)
+	
+		for i in range(curr, dest + 1):
+			display(i)
+			t.sleep(1)
+
+		for i in range(len(upled)):
+			g.output(upled[i], g.LOW)
+
+	elif(curr > dest):
+		for i in range(len(downled)):
+			g.output(downled[i], g.HIGH)
+			g.output(upled[i], g.LOW)
+
+		for i in range(curr, dest - 1 , -1):
+			display(i)
+			sleep(1)
+
+		for i in range(len(downled)):
+			g.output(downled[i], g.LOW)
+
+clearled()
+
+while True :
+	if(g.input("P8_7")==0):
+		curr = dest 
+		dest = 3
+		disp(curr, dest)
+	elif(g.input("P8_9")==0):
+		curr = dest
+		dest = 2
+		disp(curr, dest)
+	elif(g.input("P8_8")==0):
+		curr = dest 
+		dest = 1
+		disp(curr, dest)
+	elif(g.input("P8_10")==0):
+		curr = dest
+		dest = 0
+		disp(curr, dest)
 
 
-while(True):
-        #Idf ground floor key is pressed
-        if(GPIO.input('P8_10')==0):
-                clear()
-                prevFloor = currFloor
-                currFloor = 'P8_10'
-                for i in range(0, len(ZERO)):
-                        GPIO.output(ZERO[i], GPIO.LOW)
-
-        #If first floor key is pressed
-        if(GPIO.input('P8_8')==0):
-                clear()
-                prevFloor = currFloor
-                currFloor = 'P8_8'
-                for i in range(0, len(ONE)):
-                        GPIO.output(ONE[i], GPIO.LOW)
-
-        #If second floor key is pressed
-        if(GPIO.input('P8_9')==0):
-                clear()
-                prevFloor = currFloor
-                currFloor = 'P8_9'
-                for i in range(0, len(TWO)):
-                        GPIO.output(TWO[i], GPIO.LOW)
-
-        #If THIRD floor key is pressed
-        if(GPIO.input('P8_7')==0):
-                clear()
-                prevFloor = currFloor
-                currFloor = 'P8_7'
-                for i in range(0, len(THREE)):
-                        GPIO.output(THREE[i], GPIO.LOW)
